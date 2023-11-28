24ecaa72d1365dfd502c314a1bb6031c11d5f809
Pramsi <manuelprammer@gmx.at>
2023-11-28 16:09:49 +0100

    setting up

---
+++ app/src/main/java/com/cc221002/mcfinalprojectmanuelprammer/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.cc221002.mcfinalprojectmanuelprammer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val backgroundGreen = Color(0xFF85BA39)
+val backgroundWhite = Color(0xFFECECE4)
+val orange = Color(0xFFFBA02D)
+val adventureRed = Color(0xFFE63946)
