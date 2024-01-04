4a8d7a9138bf1d28fb8edc872b413941c2ac8b36
Pramsi <manuelprammer@gmx.at>
2024-01-04 10:03:01 +0100

    added comments for the Manifest and SingleTrip.kt

--- app/src/main/java/com/cc221002/mcfinalprojectmanuelprammer/data/model/SingleTrip.kt
+++ app/src/main/java/com/cc221002/mcfinalprojectmanuelprammer/data/model/SingleTrip.kt
@@ -3,7 +3,8 @@ package com.cc221002.mcfinalprojectmanuelprammer.data.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-
+// defining how my SingleTrip class is built up
+// first it is set as an entity of the table called TripsList
 @Entity(tableName = "TripsList")
 data class SingleTrip (
     val location: String,
@@ -11,5 +12,6 @@ data class SingleTrip (
     val details: String,
     val rating: String,
     val imageUri: String?,
+    // the Primary Key "id" is automatically generated and starts with 0
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
 )
