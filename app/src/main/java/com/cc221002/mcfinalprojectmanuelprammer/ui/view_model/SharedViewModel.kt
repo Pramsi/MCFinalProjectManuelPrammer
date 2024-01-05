f8c9652e4d8d3f9293e61ff939a53ff4c041609f
Pramsi <manuelprammer@gmx.at>
2024-01-05 10:30:37 +0100

    Revert "added comments for CameraState, CameraViewModel, MainViewState and SharedViewModel"
    This reverts commit e4090ab72ce0c4e056769bc0e6c4c039c6610e63.

--- app/src/main/java/com/cc221002/mcfinalprojectmanuelprammer/ui/view_model/SharedViewModel.kt
+++ app/src/main/java/com/cc221002/mcfinalprojectmanuelprammer/ui/view_model/SharedViewModel.kt
@@ -3,13 +3,10 @@ package com.cc221002.mcfinalprojectmanuelprammer.ui.view_model
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-// this ViewModel is for saving the image uri so that it can be retrieved from editing and adding
 class SharedViewModel {
-
 	private val _imageUri = MutableStateFlow<String?>("")
 	val imageUri: StateFlow<String?> = _imageUri
 
-	// this function gets an uri and saves it into the variable above (which gets used to save the uri into the database)
 	fun setImageUri(uri: String?) {
 		_imageUri.value = uri
 	}
