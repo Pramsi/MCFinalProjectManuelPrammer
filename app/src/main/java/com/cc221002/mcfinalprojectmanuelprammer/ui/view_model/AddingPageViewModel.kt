8f5d221ebaf0dd6f4b1a1b2b025765a510aba94b
Pramsi <manuelprammer@gmx.at>
2024-01-05 08:58:11 +0100

    added comments for AddingPageViewModel.kt

--- app/src/main/java/com/cc221002/mcfinalprojectmanuelprammer/ui/view_model/AddingPageViewModel.kt
+++ app/src/main/java/com/cc221002/mcfinalprojectmanuelprammer/ui/view_model/AddingPageViewModel.kt
@@ -3,6 +3,7 @@ package com.cc221002.mcfinalprojectmanuelprammer.ui.view_model
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
+// this is the ViewModel for the addingPage to save the values that are written into the Textfields
 class AddingPageViewModel:ViewModel() {
 	var location = mutableStateOf("")
 	var details = mutableStateOf("")
@@ -10,6 +11,7 @@ class AddingPageViewModel:ViewModel() {
 	var date = mutableStateOf("")
 
 
+	// this function sets all the variables to empty strings again to "delete" the data in them
 	fun deleteAddingPageViewModelEntries(){
 		location = mutableStateOf("")
 		details = mutableStateOf("")
